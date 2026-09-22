790a64601e52db62e30d386c0f5ad1e4ffe4d2de
Daniel Vasek <DanielVasek@student.unsw.edu.au>
2026-09-22 16:14:45 +1000

    finished non api work for chef claude

---
+++ chef-ai/src/Components/IngredientList.jsx
@@ -0,0 +1,24 @@
+
+export default function IngredientsList(props) {
+
+    return (
+        props.array.length > 0 &&
+                <section>
+                    <h2>Ingredients on hand:</h2>
+                    <ul className="ingredients-list" aria-live="polite">{props.array}</ul>
+
+                    {props.array.length > 3 &&
+                        <div className="get-recipe-container">
+                            <div>
+                                <h3>Ready for a recipe?</h3>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button onClick={props.toggle}>Get a recipe</button>
+                        </div>
+                    }
+                </section>
+        
+    )
+
+
+}
--- chef-ai/src/Components/Main.jsx
+++ chef-ai/src/Components/Main.jsx
@@ -1,13 +1,24 @@
 import {useState} from "react"
+import ClaudeRecipe from "./ClaudeRecipe"
+import IngredientsList from "./IngredientList"
 
 export default function Main() {
     const [ingredients, setIngredients] = useState([])
 
     const ingredientsListItems = ingredients.map((ing) => <li key={ing}>{ing}</li>)
 
+
+    const [recipeShown, setRecipeShown] = useState(false)
+
     function submitIngredients(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngrList => [...prevIngrList, newIngredient])
+
+    }
+
+    function showRecipe() {
+        setRecipeShown(prevState => !prevState)
+        console.log(recipeShown)
 
     }
 
@@ -19,6 +30,28 @@ export default function Main() {
     //     setIngredients(prevIngrList => [...prevIngrList, newIngredient])
     // }
 
+    /**
+     * Challenge: clean up our code!
+     * Let's make a couple new components to make things a
+     * little cleaner. (Notice: I'm not suggesting what we
+     * have now is bad or wrong. I'm mostly finding an excuse
+     * to get in some hands-on practice 🙂)
+     * 
+     * 1. Move the entire recipe <section> into its own
+     *    ClaudeRecipe component
+     * 2. Move the list of ingredients <section> into its
+     *    own IngredientsList component.
+     * 
+     * While you're considering how to structure things, consider
+     * where state is, think about if it makes sense or not to
+     * move it somewhere else, how you'll communicate between
+     * the parent/child components, etc.
+     * 
+     * The app should function as it currently does when you're
+     * done, so there will likely be some extra work to be done
+     * beyond what I've listed above.
+     */
+
     return (
         <main>
             <form className="add-ingredient-form" action={submitIngredients}>
@@ -28,22 +61,8 @@ export default function Main() {
             </form>
 
 
-            { ingredientsListItems.length > 0 &&
-                <section>
-                    <h2>Ingredients on hand:</h2>
-                    <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-
-                    { ingredientsListItems.length > 3 &&
-                        <div className="get-recipe-container">
-                            <div>
-                                <h3>Ready for a recipe?</h3>
-                                <p>Generate a recipe from your list of ingredients.</p>
-                            </div>
-                            <button>Get a recipe</button>
-                        </div>
-                    }
-                </section>
-            }   
+            <IngredientsList array={ingredientsListItems} toggle={showRecipe}/>            
+            {recipeShown && <ClaudeRecipe display={recipeShown}/>}
         </main>
     )
     
